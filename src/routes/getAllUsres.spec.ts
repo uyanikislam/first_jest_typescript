5a62896b76f7c487c991e29951d3f1cc7131d8c1
uyanikislam <islamuyanik@gmail.com>
2022-12-09 23:55:47 +0300

    getAll

---
+++ src/routes/getAllUsres.spec.ts
@@ -0,0 +1,8 @@
+import {Request, Response} from 'express';
+import getAllUsers from './getAllUsers';
+
+describe('Get all users request', () => {
+    let mockRequest: Partial<Request>;
+    let mockResponse: Partial<Response>;
+    let responseObject;
+})
