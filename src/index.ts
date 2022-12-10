41958a7ccef026a6aab6af23ec8971eba42ab72a
uyanikislam <islamuyanik@gmail.com>
2022-12-10 21:50:49 +0300

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import http from "http";
+import getAllUsers from './routes/getAllUsers';
 
 const app = express();
 const server = new http.Server(app);
 server.listen(3002);
+
+app.get('/users/all', getAllUsers)
--- src/routes/getAllUsres.spec.ts
+++ src/routes/getAllUsres.spec.ts
@@ -4,5 +4,32 @@ import getAllUsers from './getAllUsers';
 describe('Get all users request', () => {
     let mockRequest: Partial<Request>;
     let mockResponse: Partial<Response>;
-    let responseObject;
+    let responseObject= {};
+
+    beforeEach(() =>{
+        mockRequest={};
+        mockResponse={
+            statusCode:0,
+            send: jest.fn().mockImplementation(result=>{
+                responseObject=result;
+            })
+        }
+    })
+    test('200 - users', ()=>{
+    const expectedStatusCode=200;
+    const expectedResponse={
+        users: [{
+            name:"ali",
+            age:25
+        },{
+            name: "sevda",
+            age: 20
+        }
+        ]
+        };
+        getAllUsers(mockRequest as Request, mockResponse as Response);
+
+        expect(mockResponse.statusCode).toBe(expectedStatusCode);
+        expect(responseObject).toEqual(expectedResponse);
+    });
 })
